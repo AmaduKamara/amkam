3ca2da66617026cf755ffaa7ac7c11a8058dd7bb
Amkam <amaduskamara@gmail.com>
2021-09-15 14:56:21 +0000

    Finished texh skills, added portfolio section and footer section

---
+++ components/icons/ArrowTop.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ArrowTop = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M5 15l7-7 7 7"
+      />
+    </svg>
+  );
+};
+
+export default ArrowTop;
