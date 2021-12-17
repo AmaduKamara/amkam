ffa8d1e5456d49e95b2929234e5457922b3413f7
Amkam <amaduskamara@gmail.com>
2021-12-17 15:48:57 +0000

    Update styles

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="bg-primary px-16 py-5 shadow-xl sticky top-0 z-50">
-      <div className="container mx-auto px-16 flex justify-between items-center">
+    <div className="bg-primary py-5 shadow-xl sticky top-0 z-50">
+      <div className="px-24 flex justify-between items-center">
         <div>
           <h1 className="text-white text-4xl">
             <span className="logo-text">Dev</span> Amkam
