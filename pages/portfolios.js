72877ceebdc1ee4ae115df61e348fc915142e031
Amkam <amaduskamara@gmail.com>
2022-02-19 20:28:57 +0000

    Add basic styles on portfolio text

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Portfolios = () => {
-  return <div>My Portfolios</div>;
+  return <div className="text-white py-32">My Portfolios</div>;
 };
 
 export default Portfolios;
