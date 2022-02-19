38297bf57fc14013cbd568c81a364b6f19b2f55d
Amkam <amaduskamara@gmail.com>
2022-02-19 20:29:23 +0000

    Add basic styles on services text

--- pages/services.js
+++ pages/services.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Services = () => {
-  return <div>Services I offer</div>;
+  return <div className="text-white py-32">Services I offer</div>;
 };
 
 export default Services;
