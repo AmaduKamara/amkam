5e0be32bc03e687fcd6b5fb6619a33f4c2927ba3
Amkam <amaduskamara@gmail.com>
2022-02-19 20:28:08 +0000

    Add basic styles on about text

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div className="text-white py-32">About</div>;
+};
 
-export default About
+export default About;
