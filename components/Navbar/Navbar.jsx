ee8d2b248a2d99db903cdcbd98656d89beb1e969
Amkam <amaduskamara@gmail.com>
2022-03-10 20:03:37 +0000

    Add active styles on active nav links

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="bg-primary py-5 shadow-xl sticky top-0 z-50">
       <div className="px-24 flex justify-between items-center">
@@ -14,42 +17,78 @@ const Navbar = () => {
           <ul className="flex items-center">
             <li className="mx-5">
               <Link href="/">
-                <a className="text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin">
+                <a
+                  className={`text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin ${
+                    router.pathname === "/"
+                      ? "text-sky-500 hover:text-sky-700"
+                      : ""
+                  }`}
+                >
                   Home
                 </a>
               </Link>
             </li>
             <li className="mx-5">
               <Link href="/about">
-                <a className="text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin">
+                <a
+                  className={`text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin ${
+                    router.pathname === "/about"
+                      ? "text-sky-500 hover:text-sky-700"
+                      : ""
+                  }`}
+                >
                   About
                 </a>
               </Link>
             </li>
             <li className="mx-5">
               <Link href="/skills">
-                <a className="text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin">
+                <a
+                  className={`text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin ${
+                    router.pathname === "/skills"
+                      ? "text-sky-500 hover:text-sky-700"
+                      : ""
+                  }`}
+                >
                   Skills
                 </a>
               </Link>
             </li>
             <li className="mx-5">
               <Link href="/services">
-                <a className="text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin">
+                <a
+                  className={`text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin ${
+                    router.pathname === "/services"
+                      ? "text-sky-500 hover:text-sky-700"
+                      : ""
+                  }`}
+                >
                   Services
                 </a>
               </Link>
             </li>
             <li className="mx-5">
               <Link href="/portfolios">
-                <a className="text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin">
+                <a
+                  className={`text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin ${
+                    router.pathname === "/portfolios"
+                      ? "text-sky-500 hover:text-sky-700"
+                      : ""
+                  }`}
+                >
                   Portfolios
                 </a>
               </Link>
             </li>
             <li className="mx-5">
               <Link href="/contact">
-                <a className="text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin">
+                <a
+                  className={`text-white text-xl hover:text-sky-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-thin ${
+                    router.pathname === "/contact"
+                      ? "text-sky-500 hover:text-sky-700"
+                      : ""
+                  }`}
+                >
                   Contact
                 </a>
               </Link>
