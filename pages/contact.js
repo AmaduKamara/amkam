1e8c2e91afcd59e9d766d77a92249f962fd2aa5b
Amkam <amaduskamara@gmail.com>
2022-06-15 11:30:54 +0000

    Add left content and footer component on contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,8 @@
-import React from "react";
 import { useState } from "react";
+import { RiMapPinUserFill } from "react-icons/ri";
+import { HiOutlineMail } from "react-icons/hi";
+import { FaPhoneAlt } from "react-icons/fa";
+import Footer from "../components/Footer";
 
 const Contact = () => {
   const [fullName, setFullName] = useState("");
@@ -16,13 +19,39 @@ const Contact = () => {
   };
 
   return (
-    <>
-      <div className="text-white py-32 flex justify-center">
+    <div className="pb-24">
+      <div className="text-white pt-56 flex justify-center">
         <div className="flex justify-center w-9/12">
-          <div className="w-1/2">
+          <div className="w-1/2 mr-24">
             <h1 className="font-semibold h-text-color text-4xl md:text-5xl lg:text-7xl my-2">
               Let's Connect
             </h1>
+            <p>
+              Do you need someone with my expertise, please reach out let's get
+              started. You can reach me on the social media links at the right
+              of the screen or through the following:
+            </p>
+
+            <ul className="mt-12">
+              <li className="flex mb-4">
+                <RiMapPinUserFill className="mr-3 text-4xl h-text-color" />
+                <span className="text-xl md:text-2xl font-thin">
+                  Freetown, Sierra Leone.
+                </span>
+              </li>
+              <li className="flex mb-4">
+                <HiOutlineMail className="mr-3 text-4xl h-text-color" />
+                <span className="text-xl md:text-2xl font-thin">
+                  amaduskamara@gmail.com
+                </span>
+              </li>
+              <li className="flex mb-4">
+                <FaPhoneAlt className="mr-3 text-4xl h-text-color" />
+                <span className="text-xl md:text-2xl font-thin">
+                  +23276716997
+                </span>
+              </li>
+            </ul>
           </div>
           <div className="w-1/2 portfolio-bg-popover rounded-md shadow-xl">
             <form className="w-full p-6" onSubmit={handleSubmit}>
@@ -67,7 +96,8 @@ const Contact = () => {
           </div>
         </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
